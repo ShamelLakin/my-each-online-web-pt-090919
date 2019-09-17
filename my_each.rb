7b644f146b4ac5d5ec734390a361e9639bd11078
Shamel Lakin <christlife4u@gmail.com>
2019-09-17 13:43:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,15 @@
-def my_each(words)
+def my_each(collection)
     i = 0
  
-  while i < words.length
-     yield words[i]
+  while i < collection.length
+     yield collection[i]
 
     i = i + 1
   end
 
 end
 
-words = ['hi', 'hello', 'bye', 'goodbye']
-my_each(words) do |i|
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
   puts i
 end
